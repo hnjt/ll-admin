614d3351aa983f141b5038369e2846367dbe6a6b
CHENYB <chenyongbin@cnpc.com.cn>
2020-07-24 15:49:13 +0800

    eureka & feign init

--- src/main/java/com/ll/AdminApplication.java
+++ src/main/java/com/ll/AdminApplication.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication
 public class AdminApplication extends SpringBootServletInitializer {
 
@@ -16,4 +22,5 @@ public class AdminApplication extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources( AdminApplication.class );
     }
+
 }
--- src/test/java/com/ll/AdminApplicationTests.java
+++ src/test/java/com/ll/AdminApplicationTests.java
@@ -1,18 +1,29 @@
-//package com.ll;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import java.util.UUID;
-//
-//@SpringBootTest
-//class AdminApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//        System.out.println("ok!");
-//        String uuid = UUID.randomUUID().toString().replace( "-", "" );
-//        System.out.println(uuid);
-//        System.out.println(uuid.length());
-//    }
-//}
+package com.ll;
+
+import com.ll.admin.feign.FeignService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+    private FeignService feignService;
+
+    @Autowired
+    public void setFeignService(FeignService feignService) {
+        this.feignService = feignService;
+    }
+
+    @Test
+    void contextLoads() {
+        System.out.println("ok!");
+        String allDictionary = feignService.findAllDictionary();
+        System.out.println(allDictionary);
+        System.out.println("------------------------------");
+        String job = feignService.getJob( "7035580d45304068b0407dbf67ece1a6" );
+        System.out.println(job);
+    }
+}
