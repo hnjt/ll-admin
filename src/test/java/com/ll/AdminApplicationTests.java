086e6a46652801d10dc672291064466f2d26c948
CHENYB <chenyongbin@cnpc.com.cn>
2020-07-24 15:53:00 +0800

    eureka & feign init

--- src/test/java/com/ll/AdminApplicationTests.java
+++ src/test/java/com/ll/AdminApplicationTests.java
@@ -1,29 +1,28 @@
-package com.ll;
-
-import com.ll.admin.feign.FeignService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.UUID;
-
-@SpringBootTest
-class AdminApplicationTests {
-
-    private FeignService feignService;
-
-    @Autowired
-    public void setFeignService(FeignService feignService) {
-        this.feignService = feignService;
-    }
-
-    @Test
-    void contextLoads() {
-        System.out.println("ok!");
-        String allDictionary = feignService.findAllDictionary();
-        System.out.println(allDictionary);
-        System.out.println("------------------------------");
-        String job = feignService.getJob( "7035580d45304068b0407dbf67ece1a6" );
-        System.out.println(job);
-    }
-}
+//package com.ll;
+//
+//import com.ll.admin.feign.FeignService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//
+//@SpringBootTest
+//class AdminApplicationTests {
+//
+//    private FeignService feignService;
+//
+//    @Autowired
+//    public void setFeignService(FeignService feignService) {
+//        this.feignService = feignService;
+//    }
+//
+//    @Test
+//    void contextLoads() {
+//        System.out.println("ok!");
+//        String allDictionary = feignService.findAllDictionary();
+//        System.out.println(allDictionary);
+//        System.out.println("------------------------------");
+//        String job = feignService.getJob( "7035580d45304068b0407dbf67ece1a6" );
+//        System.out.println(job);
+//    }
+//}
